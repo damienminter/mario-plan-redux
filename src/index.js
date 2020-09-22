850448041d9e3d988dd46ee3a9c3d8130f7b379f
Damien Minter <Damo@MacBook-Pro-2.local>
2020-09-22 13:29:41 +1000

    Import redux create reducers root reducer wrap in provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// 1 import redux
+import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
+// 3 add provider level around our app
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 2 Need to add a root reducer
+const store = createStore(rootReducer);
+
+// 4. wrap with provider
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
