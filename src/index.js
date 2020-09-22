588ca0f15c88ce1b0e99c29f29cd6883688c7e81
Damien Minter <Damo@MacBook-Pro-2.local>
2020-09-22 15:29:17 +1000

    Create Project Actions

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// 1 import redux
-import { createStore } from 'redux';
+// 1 import redux and imply middleware
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 // 3 add provider level around our app
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 // 2 Need to add a root reducer
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 // 4. wrap with provider
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
