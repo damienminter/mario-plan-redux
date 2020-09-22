68f2790e36f18c9ab6cbe06a08673786a69cbd16
Damien Minter <Damo@MacBook-Pro-2.local>
2020-09-22 14:50:40 +1000

    Connect Dashboard to store inc props and update components

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import ProjectList from '../projects/ProjectList'
 import Notifications from './Notifications'
+// connect to store using react-redux (for react)
+import { connect } from 'react-redux'
 
 class Dashboard extends Component {
   render() {
+
+    // console.log(this.props);
+    // get the projects 
+    const { projects } = this.props;
+
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notifications />
@@ -19,4 +26,12 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard
+// Create a function to connect to the project store 
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects
+  }
+}
+
+// Tell connect how to connect
+export default connect(mapStateToProps)(Dashboard)
