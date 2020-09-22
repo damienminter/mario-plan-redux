ad2a42532f2d894220d6bb6e3f9c0d7963fe2c1c
Damien Minter <Damo@MacBook-Pro-2.local>
2020-09-22 15:34:55 +1000

    install Firebase

---
+++ src/config/fbconfig.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBzD0f7U4Zx1hzX5niSd1cHVn8-a__StEw",
+    authDomain: "learning-63a7a.firebaseapp.com",
+    databaseURL: "https://learning-63a7a.firebaseio.com",
+    projectId: "learning-63a7a",
+    storageBucket: "learning-63a7a.appspot.com",
+    messagingSenderId: "483138480956",
+    appId: "1:483138480956:web:5aaf3d30aeef656c700c39",
+    measurementId: "G-HBQYHYQ1KW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true })
+
+  export default firebase;
